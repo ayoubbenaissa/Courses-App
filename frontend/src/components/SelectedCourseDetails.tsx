0a26025d725b11f625fcc15cc0b3b56e34dd355b
ayoubbenaissa <ayoub.benaissa.mail@gmail.com>
2023-10-22 14:18:39 +0200

    add component to update course from details modal

--- frontend/src/components/SelectedCourseDetails.tsx
+++ frontend/src/components/SelectedCourseDetails.tsx
@@ -1,11 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import { Course } from "../types/Courses";
 import "../styles/SelectedCourseDetails.css";
 import { MouseClickEvent } from "../types/utility-types";
 
+import iconModify from "../assets/icons-modify.png";
+import { courseToUpdate$ } from "../state-signals/courseToUpdate.signal";
+
 export const SelectedCourseDetails = ({selectedCourse, closeModal}: {selectedCourse: Course, closeModal: (e: MouseClickEvent) => void}) => {
+    const navigate = useNavigate();
+
+    const handleOpenCoursePage = () => {
+        courseToUpdate$.value = selectedCourse;
+        navigate(`/courses/${selectedCourse._id}`);
+    };
+
     return (
         <div className="course-details_container" onClick={(e) => closeModal(e)}>
             <div className="course-details_modal" onClick={(e) => e.stopPropagation()}>
+                <div className="course-details-item_actions_item" onClick={() => handleOpenCoursePage()}>
+                                <img src={iconModify} alt="icon update" />
+                                <p>update course</p>
+                </div>
                 <h1 className="course-details_name">{selectedCourse.name}</h1>
                 <div className="course-details_coach">
                     <p>Coach: </p>
